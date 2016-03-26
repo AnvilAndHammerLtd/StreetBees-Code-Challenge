6e0ac1686af2fa8bb46c6d38a2c005b1a10d7d6d
Joanna-PC\Joanna <ioannaChatzittofi@gmail.com>
2016-03-26 00:15:52 +0000

    added models for API responses

---
+++ app/src/main/java/com/alexandroukyriakos/streetbeescodechallenge/models/Item.java
@@ -0,0 +1,28 @@
+
+package com.alexandroukyriakos.streetbeescodechallenge.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import javax.annotation.Generated;
+
+@Generated("org.jsonschema2pojo")
+public class Item extends Item___ {
+    @SerializedName("role")
+    @Expose
+    private String role;
+
+    /**
+     * @return The role
+     */
+    public String getRole() {
+        return role;
+    }
+
+    /**
+     * @param role The role
+     */
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
---
+++ app/src/main/java/com/alexandroukyriakos/streetbeescodechallenge/models/Item__.java
@@ -0,0 +1,28 @@
+
+package com.alexandroukyriakos.streetbeescodechallenge.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import javax.annotation.Generated;
+
+@Generated("org.jsonschema2pojo")
+public class Item__ extends Item___ {
+    @SerializedName("type")
+    @Expose
+    private String type;
+
+    /**
+     * @return The type
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * @param type The type
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+}

f8ec03206372a0902de7591069ef66cc8c8b718b
Joanna-PC\Joanna <ioannaChatzittofi@gmail.com>
2016-03-26 18:28:38 +0000

    minor names refactoring

--- app/src/main/java/com/alexandroukyriakos/streetbeescodechallenge/ui/activities/MainActivity.java
+++ app/src/main/java/com/alexandroukyriakos/streetbeescodechallenge/ui/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.alexandroukyriakos.streetbeescodechallenge;
+package com.alexandroukyriakos.streetbeescodechallenge.ui.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import com.alexandroukyriakos.streetbeescodechallenge.R;
 
 public class MainActivity extends AppCompatActivity {
 
